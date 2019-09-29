c9dc217bf3723a621218f1361793eef5cd1a36a9
Max <mlalx@tut.by>
2019-09-29 22:41:19 +0300

    feat: add implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // your solution
+    let arr = str.split('');
+    let strArr = [];
+    strArr[0] = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if ((arr[i] == "(") || (arr[i] == "{") || (arr[i] == "[") || (arr[i] == "<")) {
+            strArr.push(arr[i])
+            continue;
+        };
+        if ((arr[i] == ")") && (strArr[strArr.length - 1] == "(")) {
+            strArr.pop();
+        } else if ((arr[i] !== "}") && (arr[i] !== "]" && (arr[i] !== "|") && (arr[i] !== ">"))) {
+            strArr.push(arr[i])
+        };
+        if ((arr[i] == "}") && (strArr[strArr.length - 1] == "{")) {
+            strArr.pop();
+        } else if ((arr[i] !== ")") && (arr[i] !== "]") && (arr[i] !== "|") && (arr[i] !== ">")) {
+            strArr.push(arr[i])
+        };
+        if ((arr[i] == "]") && (strArr[strArr.length - 1] == "[")) {
+            strArr.pop();
+        } else if ((arr[i] !== "}") && (arr[i] !== ")") && (arr[i] !== "|") && (arr[i] !== ">")) {
+            strArr.push(arr[i])
+        };
+        if ((arr[i] == ">") && (strArr[strArr.length - 1] == "<")) {
+            strArr.pop();
+        } else if ((arr[i] !== "}") && (arr[i] !== ")") && (arr[i] !== "|") && (arr[i] !== "]")) {
+            strArr.push(arr[i])
+        };
+        if ((arr[i] == "|") && (strArr[strArr.length - 1] !== "|")) strArr.push(arr[i]);
+        if ((arr[i] == "|") && (strArr[strArr.length - 1] == "|")) strArr.pop();
+
+
+    }
+    return !strArr[0];
 }
